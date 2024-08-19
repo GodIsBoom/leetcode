b4f06f382e696b9bdd2e3a10d547b92b468278ec
GodIsBoom <guxukai365@gmail.com>
2024-08-20 00:27:41 +0800

    done in 10min

--- bytedance/round1_0819.py
+++ bytedance/round1_0819.py
@@ -8,3 +8,17 @@ n = len(nums2)
 
 dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
 dp[0][0] = 1
+for i in range(1, m+1):
+    for j in range(1, n+1):
+        if(nums1[i-1] == nums2[j-1]):
+            if(i == 1 and j == 1):
+                dp[i][j] = 1
+            elif(dp[i-1][j-1] > 0):
+                dp[i][j] = dp[i-1][j-1]+1
+            else:
+                dp[i][j] = 1
+ans = 0
+for i in range(1, m+1):
+    for j in range(1, n+1):
+        ans = max(ans, dp[i][j])
+print(ans)
